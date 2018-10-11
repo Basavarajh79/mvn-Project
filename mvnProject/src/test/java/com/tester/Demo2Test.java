9a50a9828e3275f076697343359830bbdfc5f178
Hp <Hp@DESKTOP-SSU3LF0>
2018-10-11 10:59:46 +0530

    mvn First

---
+++ mvnProject/src/test/java/com/tester/Demo2Test.java
@@ -0,0 +1,18 @@
+package com.tester;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test 
+{
+	@Test
+	public void test3()
+	{
+		System.out.println("executing test3");
+	}
+	@Test
+	public void test4()
+	{
+		System.out.println("executing test4");
+	}
+
+}
---
+++ mvnProject/src/test/java/com/tester/DemoTest.java
@@ -0,0 +1,18 @@
+package com.tester;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("executing test1");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("executing test2");
+	}
+
+}
